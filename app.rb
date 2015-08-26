25f3304c5553f229b7118e524446bb4a00eef21f
Tyler Machen <tylermachen@gmail.com>
2015-08-26 14:07:58 -0400

    done

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      erb :"pirates/show"
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :"pirates/new"
+    end
 
+    post '/pirates' do
+      @pirate = params[:pirate]
+    end
   end
 end
